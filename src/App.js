c134e608c99d1369b8fe8e82b96a61c738177797
Rasmus Longva Haugland <rasmus.longva.haugland@gyldendal.no>
2023-02-15 12:33:36 +0100

    Added file for image view of power source

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Slider from './components/slider';
+import SliderWithImages from "./components/SliderWithImages"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Slider />
+        <SliderWithImages/>
       </header>
     </div>
   );
--- src/components/slider.js
+++ src/components/slider.js
@@ -1,9 +1,3 @@
-import kullkraft from "../images/kullkraft.jpg"
-import vindkraft from "../images/vindkraft.jpg"
-import vannkraft from "../images/vannkraft.jpg"
-import solkraft from "../images/solkraft.jpg"
-
-
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
