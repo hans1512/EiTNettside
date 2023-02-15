63469ec25f39d2691588b32abfb165a529b0db29
Hans <hansal@ntnu.no>
2023-02-15 13:15:44 +0100

    fixing

---
+++ src/components/dropdown/dropdown.js
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import './dropdown.css'
+
+const Dropdown = ({items, type}) => {
+    const [open, setOpen] = React.useState(false);
+    const [selected, setSelected] = React.useState(type);
+
+    const handleOpen = () => {
+        setOpen(!open);
+    };
+
+    const handleDropdownOptions = (option) => {
+        // do something
+        setOpen(false);
+        setSelected(option)
+        console.log(option)
+    };
+
+
+    return (
+        <div className="dropdown">
+            <button className="dropdown-button" onClick={handleOpen}>{selected}</button>
+            {open ? (
+                <ul className="menu">
+                    {items.map((menuItem, index) => (
+                        <li key={index} className="menu-item">
+                            {React.cloneElement(menuItem, {
+                                onClick: () => {
+                                    handleDropdownOptions(menuItem.props.children)
+                                },
+                            })}
+                        </li>
+                    ))}
+                </ul>
+            ) : null}
+        </div>
+    );
+};
+
+export default Dropdown;
