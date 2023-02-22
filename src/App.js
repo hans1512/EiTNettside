ffd3b47505fc6801f9192a896fd506fb5a3301cb
Rasmus Longva Haugland <rasmus.longva.haugland@gyldendal.no>
2023-02-22 15:21:49 +0100

    Adde tabs, no functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import "./App.css";
 import React, { useState } from "react";
 import Page1 from "./pages/page1";
+import BasicTabs from "./components/tabs";
+
+
 
 function App() {
+  const [CurrentTab, SetCurretenTab] = React.useState()
+
+  const handleChangeTabs = (event, value) => {
+    SetCurretenTab(value)
+  }
+
+  const [data, setData] = useState('');
+  
+  const childToParent = (childdata) => {
+    setData(childdata);
+  }
   return (
-    <div className="App">
-      {
-        /// Add menu options/ tabs 1,2,3
-      }
-      <Page1 />
+    <div>
+      {data}
+      <BasicTabs childToParent={childToParent}/>
+      <div>
+        <Page1 />
+      </div>
     </div>
   );
 }
