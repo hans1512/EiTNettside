c94d5ffbb44841a98cf781de19b02130c5e66bb3
Hans <hansal@ntnu.no>
2023-04-12 10:17:05 +0200

    Changes

--- src/App.js
+++ src/App.js
@@ -4,23 +4,20 @@ import Page1 from "./pages/page1";
 import BasicTabs from "./components/tabs";
 import TabsFilled from "./components/TabsFilled";
 
-
 function App() {
-  const [CurrentTab, SetCurretenTab] = React.useState()
+  const [CurrentTab, SetCurretenTab] = React.useState();
 
   const handleChangeTabs = (event, value) => {
-    SetCurretenTab(value)
-  }
+    SetCurretenTab(value);
+  };
 
-  const [data, setData] = useState('');
-  
+  const [data, setData] = useState("");
+
   const childToParent = (childdata) => {
     setData(childdata);
-  }
+  };
   return (
-    
     <div>
-    <h1>SAF</h1>
       <TabsFilled />
     </div>
   );
